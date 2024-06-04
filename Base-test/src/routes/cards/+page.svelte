3d69093ce05983969cdef476313de824903c53f8
Samuel Bridges (1000099539) <BRIDSR2@student.op.ac.nz>
2024-06-04 15:18:58 +1200

    deleted website folder

--- Base-test/src/routes/cards/+page.svelte
+++ Base-test/src/routes/cards/+page.svelte
@@ -1,6 +1,16 @@
 <script>
 
-        
+
 
 </script>
 
+<video controls width="250">
+    <source src="https://www.youtube.com/watch?v=1keSFYKz8QQ" type="video/webm" />
+  
+  
+    Download the
+    <a href="/media/cc0-videos/flower.webm">WEBM</a>
+    or
+    <a href="/media/cc0-videos/flower.mp4">MP4</a>
+    video.
+  </video>
